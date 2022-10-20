3d408c1da6139cf8803767c77b5391ee832bd542
Ha Nguyen <vietha.nguyen3110@gmail.com>
2022-10-20 13:30:03 -0700

    Add Users table migration and seeds

---
+++ backend/db/seeders/20221020201752-demo-user.js
@@ -0,0 +1,46 @@
+'use strict';
+const bcrypt = require('bcryptjs')
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'use1@user.io',
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password12')
+      },
+      {
+        email: 'user2@user.io',
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password234')
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+  }
+};
