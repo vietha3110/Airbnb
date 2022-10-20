5ae8d003a0d13aa85407ec67dc1dbfeadb980dcb
Ha Nguyen <vietha.nguyen3110@gmail.com>
2022-10-20 14:53:15 -0700

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,8 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth.js');
 
 
 // backend/routes/api/index.js
@@ -10,6 +13,26 @@ router.post('/test', function(req, res) {
   });
   
 // ...
+// GET /api/set-token-cookie test user auth middleware
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       }
+//     });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// });
+
+// router.use(restoreUser);
+
+// router.get(
+//   '/restore-user',
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
 
 
 module.exports = router;
---
+++ backend/utils/auth.js
@@ -0,0 +1,67 @@
+const jwt = require('jsonwebtoken');
+const { jwtConfig } = require('../config');
+const { User } = require('../db/models');
+
+const { secret, expiresIn } = jwtConfig;
+
+//Sents a JWT cookie
+
+const setTokenCookie = (res, user) => {
+    //create TOKEN here:
+    const token = jwt.sign({
+        data: user.toSafeObject()
+    },
+        secret,
+        {
+            expiresIn: parseInt(expiresIn)
+        }
+    );
+    
+    const isProduction = process.env.NODE_ENV === 'production';
+    //set the token cookie 
+    res.cookie('token', token, {
+        maxAge: expiresIn * 1000, //maxAge in milliseconds 
+        httpOnly: true,
+        secure: isProduction,
+        sameSite: isProduction && 'Lax'
+    });
+
+    return token;
+};
+//certain authenticated routes require the identity of current session user.
+//create & utilize a middleware function called restoreUser => restore the session user based on the contents of the JWT cookie
+const restoreUser = (req, res, next) => {
+    //token parsed from cookies
+    const { token } = req.cookies;
+    req.user = null;
+    return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+        if (err) {
+            return next();
+        }
+
+        try {
+            const { id } = jwtPayload.data;
+            req.user = await User.scope('currentUser').findByPk(id);
+        } catch (e) {
+            res.clearCookie('token');
+            return next();
+        }
+
+        if (!req.user) res.clearCookie('token');
+
+        return next();
+    });
+};
+
+//if there is no current user, return error 
+const requireAuth = function (req, _res, next) {
+    if (req.user) {
+        return next();
+    }
+    const err = new Error('Unauthorized'); 
+    err.title = 'Unauthorized';
+    err.errors = ['Unauthorized'];
+    err.status = 401;
+    return next(err);
+}
+module.exports = { setTokenCookie, restoreUser, requireAuth };
