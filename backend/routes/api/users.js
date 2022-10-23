0c402acaac744abddabaf59dfb655f4c2c27119c
Ha Nguyen <vietha.nguyen3110@gmail.com>
2022-10-23 12:36:43 -0700

    Update error-response for sign up user

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -11,25 +11,25 @@ const validateSignup = [
     check('email')
         .exists({ checkFalsy: true })
         .isEmail()
-        .withMessage('Please provide a valid email.'),
+        .withMessage('Invalid email'),
     check('username')
         .exists({ checkFalsy: true })
         .isLength({ min: 4 })
-        .withMessage('Please provide a username with at least 4 characters.'),
+        .withMessage('Username is required'),
     check('username')
         .not()
         .isEmail()
-        .withMessage('Username cannot be an email.'),
+        .withMessage('Username cannot be an email'),
     check('password')
         .exists({ checkFalsy: true })
         .isLength({ min: 6 })
-        .withMessage('Password must be 6 characters or more.'),
+        .withMessage('Password must be 6 characters or more'),
     check('firstName')
         .exists({ checkFalsy: true })
-        .withMessage('Please provide your first name.'),
+        .withMessage('First Name is required'),
     check('lastName')
         .exists({ checkFalsy: true })
-        .withMessage('Please provide your last name.'),
+        .withMessage('Last Name is required'),
     handleValidationErrors
 ];
 
