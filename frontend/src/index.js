9ad2210c04c6b17d903cd9cac2a36cdf46e1c53b
Ha Nguyen <vietha.nguyen3110@gmail.com>
2022-11-10 21:37:57 -0800

    innitialized frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './index.css';
+import {createRoot} from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+)
