3005eded86bf39f8e86e557e8b38420dc4f0d33e
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-02-26 23:31:27 -0800

    Turn off csurf

--- backend/app.js
+++ backend/app.js
@@ -31,17 +31,17 @@ app.use(
     policy: "cross-origin" 
   })
 );
-  
+
   // Set the _csrf token and create req.csrfToken method
-app.use(
-  csurf({
-    cookie: {
-      secure: isProduction,
-      sameSite: isProduction && "Lax",
-      httpOnly: true
-    }
-  })
-);
+// app.use(
+//   csurf({
+//     cookie: {
+//       secure: isProduction,
+//       sameSite: isProduction && "Lax",
+//       httpOnly: true
+//     }
+//   })
+// );
 
 
 // add routes 
